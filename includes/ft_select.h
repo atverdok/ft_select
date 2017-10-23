ef09b828447d1c27024c584a1b10fccb3932cab7
atverdok <lexa_dpp@ukr.net>
2017-10-23 22:49:09 +0300

    home

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -5,6 +5,8 @@
 #ifndef FT_SELECT_H
 # define FT_SELECT_H
 
+#include <stdio.h>
+
 # include <termcap.h>
 # include <sys/ioctl.h>
 # include <termios.h>
@@ -13,5 +15,17 @@
 
 pid_t	g_pid;
 struct termios	g_saved_attributes;
+
+typedef struct s_in_arg
+{
+    int             select;
+    char            *value;
+    struct s_in_arg *next;
+    struct s_in_arg *prew;
+}              t_in_arg;
+
+void	reset_input_mode(void);
+void	save_attributes(void);
+void	set_input_mode(void);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,8 +1,45 @@
-#include <stdio.h>
 
 #include "../includes/ft_select.h"
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int tputs_putchar(int c)
+{
+    write(1, &c, 1);
+    return (1);
+}
+
+//int main()
+//{
+//    struct termios	term;
+//    int	height;
+//    int	width;
+//
+//    save_attributes();
+//    set_input_mode();
+//    height = tgetnum ("li");
+//    width = tgetnum ("co");
+//    tputs(tgetstr("cl", NULL), 0, tputs_putchar);
+//    printf("Hello, World!\n");
+//    reset_input_mode();
+//    return 0;
+//}
+
+int     main()
+{
+//    const char *name;
+    char  *bp;
+    char  *term;
+    int   height;
+    int   width;
+
+    bp = malloc(sizeof(*bp));
+//    name = "TERM";
+    if ((term = getenv("TERM")) == NULL)
+        return (1);
+    printf("My terminal is %s.\n", term);
+    tgetent(bp, term);
+    height = tgetnum ("li");
+    width = tgetnum ("co");
+    printf("H : %d\nL : %d\n", height, width);
+    free(bp);
+    return (0);
 }
