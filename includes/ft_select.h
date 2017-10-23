52b86ff403360d1207c415058b9a7fb4297ece61
atverdok <lexa_dpp@ukr.net>
2017-10-23 17:18:48 +0300

    11

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -5,6 +5,13 @@
 #ifndef FT_SELECT_H
 # define FT_SELECT_H
 
+# include <termcap.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+
 # include "../libft/libft.h"
+
+pid_t	g_pid;
+struct termios	g_saved_attributes;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "../includes/ft_select.h"
+
 int main() {
     printf("Hello, World!\n");
     return 0;
